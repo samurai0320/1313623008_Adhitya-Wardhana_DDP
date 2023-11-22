e682b904d8c0a0b918603a58d68239b204539a82
samurai0320 <147383027+samurai0320@users.noreply.github.com>
2023-11-22 07:26:19 +0700

    Add files via upload

---
+++ Sorting.d
@@ -0,0 +1,17 @@
+module Sorting;
+
+import std.stdio;
+import std.algorithm;
+
+
+void main() {
+    int[] value = [949, 150, 896, 648, 106, 289, 703, 376, 758, 46, 799, 190, 427, 474, 853, 589, 532,333, 234, 1005];
+    int[] list = new int[value.length];
+
+    makeIndex!"a<b"(value,list);
+    
+    foreach (i,index; list) {
+        write(index+1, " ");
+    }
+    writeln();
+}
